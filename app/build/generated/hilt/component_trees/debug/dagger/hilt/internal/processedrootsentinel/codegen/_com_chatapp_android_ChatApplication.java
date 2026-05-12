0bd09587ba2baaae526b3c05b125888bf93d6f34
THARUN RAJ R <tharunrajr2004@gmail.com>
2026-05-12 18:31:56 +0530

    chore: remove exposed build directory and update gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_chatapp_android_ChatApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.chatapp.android.ChatApplication"
-)
-public final class _com_chatapp_android_ChatApplication {
-}
